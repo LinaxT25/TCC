a5583d7cc2d0fee33c356f0b7ee9e07a2606002e
Vinícius Galvão <viniciusgalvao2000@gmail.com>
2023-08-23 19:32:12 -0400

    Graphs fixed to compare with new patterns and rules.

--- FP_Growth/FPGrowth.py
+++ FP_Growth/FPGrowth.py
@@ -8,9 +8,11 @@ from FP_Growth import Graphs
 def fp_growth():
     activity_list = ActivitiesExtract.data_pattern()
     # First analysis
-    patterns = pyfpgrowth.find_frequent_patterns(activity_list, 1)
-    rules = pyfpgrowth.generate_association_rules(patterns, 0.1)
-    Graphs.graphs(patterns, rules)
+    patterns = pyfpgrowth.find_frequent_patterns(activity_list, 3)
+    rules = pyfpgrowth.generate_association_rules(patterns, 0.3)
+
+    Graphs.graph_pattern(patterns, 3)
+    Graphs.graph_rule(rules)
 
     # Creating a new directory for data to be exported
     dir_path = os.getcwd()
@@ -32,8 +34,11 @@ def fp_growth():
     f.close()
 
     # Second analysis
-    patterns = pyfpgrowth.find_frequent_patterns(activity_list, 2)
+    patterns = pyfpgrowth.find_frequent_patterns(activity_list, 10)
     rules = pyfpgrowth.generate_association_rules(patterns, 0.5)
+
+    Graphs.graph_pattern(patterns, 10)
+    Graphs.graph_rule(rules)
 
     # Writing to a file patterns output
     f = open("./Pattern_Output/pattern2_output.txt", "wt")
@@ -48,8 +53,11 @@ def fp_growth():
     f.close()
 
     # Third analysis
-    patterns = pyfpgrowth.find_frequent_patterns(activity_list, 3)
-    rules = pyfpgrowth.generate_association_rules(patterns, 0.7)
+    patterns = pyfpgrowth.find_frequent_patterns(activity_list, 20)
+    rules = pyfpgrowth.generate_association_rules(patterns, 0.75)
+
+    Graphs.graph_pattern(patterns, 20)
+    Graphs.graph_rule(rules)
 
     # Writing to a file patterns output
     f = open("./Pattern_Output/pattern3_output.txt", "wt")
--- FP_Growth/Graphs.py
+++ FP_Growth/Graphs.py
@@ -1,10 +1,27 @@
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 
 
-def graphs(patterns, rules):
+def graph_pattern(patterns, frequency):
+    # Patterns graph
     df = pd.DataFrame({"Activities": patterns.keys(), "Frequency": patterns.values()})
-    df.loc[df['Frequency'] < 20, 'Activities'] = 'Minor Activities'
+    df.loc[df["Frequency"] < frequency * 3, 'Activities'] = 'Minor Activities'
 
     fig = px.pie(df, values="Frequency", names="Activities", title="Github user activity")
+    fig.show()
+
+
+def graph_rule(rules):
+    # Rules graph
+    activity_chance = []
+    probability = []
+    rules_values = rules.values()
+
+    for x in rules_values:
+        activity_chance.append(x[0])
+        probability.append(x[1])
+
+    df = pd.DataFrame({"Activities": rules.keys(), "Activity_chance": activity_chance, "Probability": probability})
+    fig = px.scatter(df, x="Probability", hover_data="Activity_chance", color="Activities", size="Probability")
     fig.show()
